7eef18f3c95d9701b922f3040f9728faa5c29b9d
rmiddle <robert@middleswarth.net>
2010-04-04 17:53:34 -0400

    Tring to add back in the filter

--- features/feg.core/api/uri/customer.php
+++ features/feg.core/api/uri/customer.php
@@ -207,7 +207,7 @@ class FegCustomerTabRecipient extends Extension_CustomerTab {
 
 		$view = Feg_AbstractViewLoader::getView($defaults->id, $defaults);
 		$view->params = array(
-//			SearchFields_CustomerRecipient::ACCOUNT_ID => new DevblocksSearchCriteria(SearchFields_CustomerRecipient::ACCOUNT_ID,DevblocksSearchCriteria::OPER_EQ, $customer_id)
+			SearchFields_CustomerRecipient::ACCOUNT_ID => new DevblocksSearchCriteria(SearchFields_CustomerRecipient::ACCOUNT_ID,DevblocksSearchCriteria::OPER_EQ, sprintf("%d",$customer_id))
 		);
 		$view->renderPage = 0;
 		Feg_AbstractViewLoader::setView($view->id,$view);
