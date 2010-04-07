7a69af92e0ac3cc741e3ef5c9f6ea8f82e0fccd5
rmiddle <robert@middleswarth.net>
2010-04-06 22:32:54 -0400

    More changes.

--- features/feg.core/api/uri/customer.php
+++ features/feg.core/api/uri/customer.php
@@ -207,7 +207,7 @@ class FegCustomerTabRecipient extends Extension_CustomerTab {
 
 		$view = Feg_AbstractViewLoader::getView($defaults->id, $defaults);
 		$view->params = array(
-//SearchFields_CustomerRecipient::ACCOUNT_ID => new DevblocksSearchCriteria(SearchFields_CustomerRecipient::ACCOUNT_ID,'=',$customer_id),
+			SearchFields_CustomerRecipient::ACCOUNT_ID => new DevblocksSearchCriteria(SearchFields_CustomerRecipient::ACCOUNT_ID,'=',$customer_id),
 		);
 		$view->renderPage = 0;
 		Feg_AbstractViewLoader::setView($view->id,$view);
