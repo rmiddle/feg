ced53985dd6d72491ad344431a029ad62e972843
rmiddle <robert@middleswarth.net>
2010-04-07 08:13:27 -0400

    More Debugging

--- features/feg.core/api/uri/setup.php
+++ features/feg.core/api/uri/setup.php
@@ -442,13 +442,15 @@ class FegSetupPage extends FegPageExtension  {
 		
 		$defaults->renderSortBy = SearchFields_CustomerRecipient::ID;
 		$defaults->renderSortAsc = 0;
-
+echo "<pre>";
+print_r($defaults);
 		$view = Feg_AbstractViewLoader::getView($defaults->id, $defaults);
 		$view->params = array(
 		);
 		$view->renderPage = 0;
 		Feg_AbstractViewLoader::setView($view->id,$view);
-		
+print_r($view);		
+echo "</pre>";
 		$tpl->assign('view', $view);
 		$tpl->assign('view_fields', View_CustomerRecipient::getFields());
 		$tpl->assign('view_searchable_fields', View_CustomerRecipient::getSearchFields());
