fd234f703b1935886a7c6f3e721d2297208867c9
rmiddle <robert@middleswarth.net>
2010-04-13 20:55:34 -0400

    Update the Application ID.

--- api/Application.class.php
+++ api/Application.class.php
@@ -10,7 +10,7 @@
 | ______________________________________________________________________
 |	http://www.feg.com	  http://www.webgroupmedia.com/
 ***********************************************************************/
-define("APP_BUILD", 2010041301);
+define("APP_BUILD", 2010041302);
 
 require_once(APP_PATH . "/api/DAO.class.php");
 require_once(APP_PATH . "/api/Model.class.php");
