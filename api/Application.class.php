b77a3a53bb5f2addf884541cfa83dc03d92e9888
rmiddle <robert@middleswarth.net>
2010-04-15 01:32:41 -0400

    Update release to force update.

--- api/Application.class.php
+++ api/Application.class.php
@@ -10,7 +10,7 @@
 | ______________________________________________________________________
 |	http://www.feg.com	  http://www.webgroupmedia.com/
 ***********************************************************************/
-define("APP_BUILD", 2010041501);
+define("APP_BUILD", 2010041502);
 
 require_once(APP_PATH . "/api/DAO.class.php");
 require_once(APP_PATH . "/api/Model.class.php");
