5590739e6328a1b0ddf24a24a3c325f19109be86
rmiddle <robert@middleswarth.net>
2010-04-25 22:23:01 -0400

    More testing.

--- features/feg.core/api/uri/stats.php
+++ features/feg.core/api/uri/stats.php
@@ -100,7 +100,7 @@ class FegStatsPage extends FegPageExtension {
 		$message = DAO_Message::get($id);
 		echo "<pre>";
 		echo "message = ";
-		echo stripslashes($message->message);
+		echo $message->message;
 		echo "</pre>";
 		$tpl->assign('message', $message);
 
