b013f1e3f397c4498389dae59e6850e59c8df587
rmiddle <robert@middleswarth.net>
2010-06-06 15:54:31 -0400

    Fix typo.

--- features/feg.core/api/uri/stats.php
+++ features/feg.core/api/uri/stats.php
@@ -166,7 +166,7 @@ class FegStatsPage extends FegPageExtension {
 		echo "FaxQue: ";
 		//echo "FIXME";
 		exec("/usr/bin/faxstat -s", $results); 
-		echo implode("<br>", $result);
+		echo implode("<br>", $results);
 		echo "<br>";
 		//$tpl->display('file:' . $this->_TPL_PATH . 'stats/postfix.tpl');
 	}
