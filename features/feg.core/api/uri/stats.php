156779a5c8fc16134a4a62f5b869f17c8c4574ea
rmiddle <robert@middleswarth.net>
2010-07-05 00:28:13 -0400

    More chagnes to stats

--- features/feg.core/api/uri/stats.php
+++ features/feg.core/api/uri/stats.php
@@ -183,7 +183,8 @@ class FegStatsPage extends FegPageExtension {
 		array_shift($output_current); 		// HylaFAX scheduler on ...
 		foreach ($output_current as $line) {
 			if (preg_match("/^Modem /", $line)) {	// match "/^Modem/
-				echo $line . "<br>";
+				$arr = split(" ", $line);
+				echo implode("<br>", $arr);
 				array_shift($output_current);				// remove entry from array
 			} else {
 				break;
