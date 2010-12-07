ca5599891d9ec5cd6dd61aebe9238b007f1decc9
Robert Middleswarth <robert@middleswarth.net>
2010-12-07 06:23:46 -0500

    Lets see if I get the data to display.

--- features/feg.core/api/uri/stats.php
+++ features/feg.core/api/uri/stats.php
@@ -143,6 +143,7 @@ class FegStatsPage extends FegPageExtension {
 		$tpl = DevblocksPlatform::getTemplateService();
 		$tpl->assign('path', $this->_TPL_PATH);
 		
+echo "test";		
 		$tpl->assign('id', $id);
 		$tpl->assign('view_id', $view_id);
 		
@@ -156,7 +157,7 @@ class FegStatsPage extends FegPageExtension {
 		$custom_field_values = DAO_CustomFieldValue::getValuesBySourceIds(FegCustomFieldSource_MessageRecipient::ID, $id);
 		if(isset($custom_field_values[$id]))
 			$tpl->assign('custom_field_values', $custom_field_values[$id]);
-echo "test";		
+			
 		$tpl->display('file:' . $this->_TPL_PATH . 'setup/tabs/message_recipient/failed_peek.tpl');
 	}
 	
