61e5eaaea9908249a3c06ebea3894f6a849e601d
Robert Middleswarth <robert@middleswarth.net>
2010-12-21 08:17:55 -0500

    Minor fix to the command line.

--- api/app/Snpp.php
+++ api/app/Snpp.php
@@ -10,7 +10,7 @@ class FegSnpp {
 	static function sendSnpp($phone_number, $message, $snpp_server="ann100sms01.answernet.com", $port=444) {
 		try {
 			// snpp -s ann100sms01.answernet.com:444 -n -m "actual message" phone_number
-			$command = "snpp -s " . $snpp_server . ":" . $port . ' -n -m "'. $message .'" ' . $phone_number . '2>&1';
+			$command = "snpp -s " . $snpp_server . ":" . $port . ' -n -m "'. $message .'" ' . $phone_number . ' 2>&1';
 			$o = exec($command, $snpp_output, $retval);
 
 echo "<pre>";
