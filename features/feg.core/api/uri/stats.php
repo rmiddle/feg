9afa03277a0fe78a47edb6a746db1ae34e488741
Robert Middleswarth <robert@middleswarth.net>
2010-12-22 15:08:56 -0500

    Fix status string mistake

--- features/feg.core/api/uri/stats.php
+++ features/feg.core/api/uri/stats.php
@@ -315,8 +315,8 @@ class FegStatsPage extends FegPageExtension {
 				"inner join customer_recipient cr on mr.recipient_id = cr.id ".
 				"WHERE cr.is_disabled = 0 ".
 				"AND cr.is_disabled = 0 ".
-				"AND cr.type = 1 "
-				"AND ((mr.send_status = 5) or (mr.send_status BETWEEN 100 AND 140))".
+				"AND cr.type = 1 ".
+				"AND ((mr.send_status = 5) or (mr.send_status BETWEEN 100 AND 140))"
 				);
 		$rs = $db->Execute($sql);
 		$row = mysql_fetch_assoc($rs);
