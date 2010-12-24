274e8748138cbee54263718c66b749b923801551
Robert Middleswarth <robert@middleswarth.net>
2010-12-23 23:32:02 -0500

    Update version info to show the site in now considered beta.

--- api/Application.class.php
+++ api/Application.class.php
@@ -10,7 +10,7 @@
 | ______________________________________________________________________
 |	http://www.feg.com	  http://www.webgroupmedia.com/
 ***********************************************************************/
-define("APP_BUILD", 2010122203);
+define("APP_BUILD", 2010122301);
 
 require_once(APP_PATH . "/api/DAO.class.php");
 require_once(APP_PATH . "/api/Model.class.php");
