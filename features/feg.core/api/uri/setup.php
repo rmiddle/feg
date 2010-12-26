279ad8088f9b1dcb80d9375e217e89288ed88d3f
Robert Middleswarth <robert@middleswarth.net>
2010-12-26 09:08:41 -0500

    Add some debugging info

--- features/feg.core/api/uri/setup.php
+++ features/feg.core/api/uri/setup.php
@@ -615,6 +615,15 @@ class FegSetupPage extends FegPageExtension  {
 		$export_type = DAO_ExportType::get($id);
 		$export_type_params = DAO_ExportTypeParams::getAll();
 		
+echo "<pre>";
+echo "<br>id: "
+print_r($id);
+echo "<br>add_id: "
+print_r($add_id);
+echo "<br>export_type: "
+print_r($export_type);
+echo "<br>export_type_params: "
+print_r($export_type_params);
 		if (!exists($export_type_params[$add_id])) {
 			// Bad add_id
 			return;
@@ -633,7 +642,11 @@ class FegSetupPage extends FegPageExtension  {
 		$fields = array(
 			DAO_ExportType::PARAMS => $export_type->params,
 		);
-		
+echo "<br>export_param_addexport_param_add"
+print_r($export_param_add);
+echo "<br>fields: "
+print_r($fields);
+echo "</pre>";
 		//$status = DAO_ExportType::update($id, $fields);
 		
 		echo json_encode($export_param_add);		
