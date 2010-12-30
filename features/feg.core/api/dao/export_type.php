cbe8980106876900b39b8d5dcb02de74290d7bdc
Robert Middleswarth <robert@middleswarth.net>
2010-12-30 00:30:53 -0500

    More debugging output

--- features/feg.core/api/dao/export_type.php
+++ features/feg.core/api/dao/export_type.php
@@ -40,6 +40,7 @@ class DAO_ExportType extends Feg_ORMHelper {
 			$fields['params_json'] = json_encode($fields['params']);
 			unset($fields['params']);
 		}
+print_r($fields);
 		parent::_update($ids, 'export_type', $fields);
 	}
 	
