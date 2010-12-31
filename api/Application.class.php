9b91de6154a9b36e341d506b7983d800d7027d9b
Robert Middleswarth <robert@middleswarth.net>
2010-12-31 14:33:10 -0500

    Add back in mistakely removed file.

--- api/Application.class.php
+++ api/Application.class.php
@@ -10,7 +10,7 @@
 | ______________________________________________________________________
 |	http://www.feg.com	  http://www.webgroupmedia.com/
 ***********************************************************************/
-define("APP_BUILD", 2010122801);
+define("APP_BUILD", 2010123101);
 
 require_once(APP_PATH . "/api/DAO.class.php");
 require_once(APP_PATH . "/api/Model.class.php");
---
+++ features/feg.core/templates/customer/tabs/recipient/select_export_type.tpl
@@ -0,0 +1,8 @@
+<select name="recipient_export_type">
+  {foreach from=$export_type item=export name=export_id}
+    {if $type == $export->recipient_type}
+      <option value="{$export->id}" {if $selected_type == $export->id}selected{/if}>{$export->name}</option>
+    {/if}
+  {/foreach}
+</select>
+
