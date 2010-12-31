6f1e19a8d4359b92ef5a7814b62a6fdacee0fd26
Robert Middleswarth <robert@middleswarth.net>
2010-12-31 16:00:51 -0500

    Lets see if the DB update now

--- features/feg.core/patches/1.0.0_rc1.php
+++ features/feg.core/patches/1.0.0_rc1.php
@@ -11,7 +11,7 @@ if(!isset($tables['message']))
 list($columns, $indexes) = $db->metaTable('message');
 
 // Add import status
-if(isset($columns['import_status'])) {
+if(!isset($columns['import_status'])) {
 	$db->Execute("ALTER TABLE message ADD COLUMN import_status TINYINT UNSIGNED DEFAULT 0 NOT NULL");
 	$db->Execute("ALTER TABLE message ADD INDEX import_status (import_status)");
 }
